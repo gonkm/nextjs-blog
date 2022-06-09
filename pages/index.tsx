8aa3adfbd63d82a6243ca394f25bde1ebfc09865
Takeshi Nakamura <nkmuum@MacBookPro53.local>
2022-06-09 14:43:59 +0900

    2nd commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn/foundations/about-nextjs/what-is-nextjs">our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
