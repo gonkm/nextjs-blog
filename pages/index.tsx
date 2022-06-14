ef8aa382e6bb9a5eb79ff39d4963167d8d9ba083
gonkm <tmge017@gmail.com>
2022-06-14 09:02:29 +0900

    環境変数追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home({
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction hoge ]</p>
+        <p>{`環境は${process.env.NEXT_PUBLIC_STAGE}です`}</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn/foundations/about-nextjs/what-is-nextjs">our Next.js tutorial</a>.)
